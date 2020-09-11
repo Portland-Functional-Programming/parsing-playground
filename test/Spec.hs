f86bcc3eaafcabb8aaae785c333a71ec9889cd7b
Tim McIver <tim@timmciver.com>
2020-09-10 23:00:36 -0400

    Refactored and added to tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,18 +8,32 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Parser Tests"
-  [ testCase "successfully parse a single character" $
-    (char 'a') "abc" @?= Right ('a', "bc")
-  , testCase "fails to parse a single character" $
-    assertFailedParse ((char 'b') "abc")
+  [ testGroup "char parser tests"
+    [ testCase "successfully parse a single character" $
+      (char 'a') "abc" @?= Right ('a', "bc")
+
+    , testCase "fails to parse a single character" $
+      assertFailedParse ((char 'b') "abc")
+    ]
+
   , testCase "parser that always succeeds with value" $
     (always 'z') "abc" @?= Right ('z', "abc")
-  , testCase "parses several characters" $
-    (zeroOrMore (char 'a')) "aabc" @?= Right ("aa", "bc")
-  , testCase "parses nothing but still succeeds" $
-    (zeroOrMore (char 'a')) "bcd" @?= Right ("", "bcd")
-  , testCase "parses several characters" $
-    (oneOrMore (char 'a')) "aabc" @?= Right ('a' :| ['a'], "bc")
+
+  , testGroup "zeroOrMore parser tests"
+    [ testCase "parses several characters" $
+      (zeroOrMore (char 'a')) "aabc" @?= Right ("aa", "bc")
+
+    , testCase "parses nothing but still succeeds" $
+      (zeroOrMore (char 'a')) "bcd" @?= Right ("", "bcd")
+    ]
+
+  , testGroup "oneOrMore parser tests"
+    [ testCase "parses several characters" $
+      (oneOrMore (char 'a')) "aabc" @?= Right ('a' :| ['a'], "bc")
+    , testCase "fails to parse several one or more characters" $
+      assertFailedParse ((oneOrMore (char 'a')) "bcd")
+    ]
+
   , testGroup "either parser tests"
     [ testCase "parses either of two characters (1)" $
       (P.either (char 'a') (char 'b')) "abc" @?= Right ('a', "bc")
